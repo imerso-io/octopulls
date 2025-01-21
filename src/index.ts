f78abad14cbd3755525911405fd1da85a10175cd
lucasgianine <lucas.esilva@sptech.school>
2025-01-21 19:42:52 -0300

    Cria função para obter arquivos de um PR específico

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { logger, postCommentOnGithub } from './libs'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { logger } from './libs'
 
-postCommentOnGithub()
-  .then((response) => { logger.info(response) })
-  .catch((error) => { logger.error(error) })
+;(async function run () {
+  try {
+  const token = core.getInput('PAT_TOKEN')
+  const octokit = github.getOctokit(token)
+  const pullRequest = github.context.payload.pull_request
+
+  if (!pullRequest) throw core.error
+
+  const pullNumber = pullRequest.number
+
+  const { owner, repo } = github.context.repo
+
+  await octokit.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number: pullNumber
+  })
+  } catch (err) {
+    logger.error(err)
+  }
+})()
